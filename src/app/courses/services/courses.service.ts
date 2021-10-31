4f3a91fdb5bfcbb52c68a5776200c9052c8ff150
João <jpos.silva@gmail.com>
2021-10-31 17:37:38 -0300

    Adição de um Spinner para mostrar o símbolo de Carregando enquanto o front-end ainda não recebeu a lista de cursos da API.

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs/operators';
 
 import { Course } from '../courses/models/Course';
 
@@ -13,6 +14,12 @@ export class CoursesService {
   constructor(private httpClient: HttpClient) { }
 
   list() {
-    return this.httpClient.get<Course[]>(this.API);
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      delay(5000),
+      tap(courses => console.log(courses))
+    );
   }
 }
+
