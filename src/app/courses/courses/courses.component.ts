612d8b2fb0aef181d8bb0cb361d2eee4903a5a9f
João <jpos.silva@gmail.com>
2021-11-01 20:46:21 -0300

    Adição de um MatDialog para mostrar uma mensagem de erro. Tratamento de erros em chamadas HTTP com RxJS.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
 import { CoursesService } from '../services/courses.service';
 import { Course } from './models/Course';
 
@@ -13,12 +17,24 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) { 
-    this.courses$ = this.coursesService.list();
+  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos.')
+          return of([])
+        })
+      );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
-    
+
   }
 
 }
